4490ddf37e4caedfd3b76b567675f0f6b0a9417b
etiennepayet <etienne.payet@univ-reunion.fr>
2023-08-02 11:39:47 +0400

    Create payet-nonloop-4_1.pl

---
+++ Logic_Programming/Payet_23/payet-nonloop-4_1.pl
@@ -0,0 +1,7 @@
+% The following mode is non-terminating.
+%query: p(i,i).
+
+p(X, []) :-
+    p(g(X, h(0), X), g(g(g(X, h(0), X), h(0), g(X, h(0), X)), h(0), g(g(X, h(0), X), h(0), g(X, h(0), X)))).
+p(X, g(Y, h(0), Y)) :-
+    p(g(g(X, h(0), X), h(0), g(X, h(0), X)), Y).
